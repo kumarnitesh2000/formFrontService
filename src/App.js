b0e1446227c4cd2cc8bd34aab98a45df59e6550a
kumarnitesh2000 <46937660+kumarnitesh2000@users.noreply.github.com>
2020-08-27 07:45:33 -1000

    template demo

--- src/App.js
+++ src/App.js
@@ -12,7 +12,83 @@ const are_you_testing = false;
 //use that obj to create form template in DynamicForm component  .
 // TODO
 const template = {
-
+  "form": {
+    "form_title": "Contact Form",
+    "form_description": "Please Enter Your Contact Details Very Carefully . if wrong contact then no selection"
+  },
+  "section": {
+    "section_title": [
+      "Contact Section",
+      "Personal Info Section",
+      "Resume Section"
+    ],
+    "section_description": [
+      "In this Section Phone number and Mail Are there Please Fill these correctly",
+      "In This Section Your Name  , Father Name , Mothers Name are there , Gender",
+      "In this Section your CV , graduation Year |Month , Your Skills"
+    ]
+  },
+  "field": {
+    "field_description": [
+      "phone",
+      "email",
+      "Enter Name",
+      "Fathers Name",
+      "Gender",
+      "CV",
+      "Graduation Year | Month"
+    ],
+    "field_label": [
+      "phone",
+      "email",
+      "Name",
+      "Fathers Name",
+      "Gender",
+      "CV",
+      "Graduation Year | Month"
+    ],
+    "field_type_list": [
+      {
+        "field_type": "number",
+        "index": 0,
+        "options": []
+      },
+      {
+        "field_type": "text",
+        "index": 1,
+        "options": []
+      },
+      {
+        "field_type": "text",
+        "index": 2,
+        "options": []
+      },
+      {
+        "field_type": "text",
+        "index": 3,
+        "options": []
+      },
+      {
+        "field_type": "dropDown",
+        "index": 4,
+        "options": [
+          "Male",
+          "female",
+          "others"
+        ]
+      },
+      {
+        "field_type": "fileUpload",
+        "index": 5,
+        "options": []
+      },
+      {
+        "field_type": "date&time",
+        "index": 6,
+        "options": []
+      }
+    ]
+  }
 };
 return(
 <div className="App">
